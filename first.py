8a49b783ddb5ed6883c1aaa1b6a33e9b89602c33
Mike K <michaelkucukarslan@gmail.com>
2024-01-29 22:13:21 -0800

    Got everything but the rating

--- first.py
+++ first.py
@@ -1,6 +1,34 @@
-import requests
+import requests, csv
+from bs4 import BeautifulSoup
 
 URL = "http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"
 page = requests.get(URL)
 
-print(page.text)
+soup = BeautifulSoup(page.text, 'html.parser')
+
+# Most of the needed information is in the table towards the bottom. Find all the tds and place them in the appropriate section
+coloumns = soup.find_all('td')
+upc = coloumns[0].text.strip()
+price_including_tax = coloumns[2].text.strip()
+price_excluding_tax = coloumns[3].text.strip()
+quantity_available = coloumns[5].text.strip()
+
+# Other information was around the html page
+product_page_url = URL
+category_of_book = soup.find_all('a')[3].text
+title = soup.find('h1').text 
+product_description = soup.find_all('p')[3].text 
+image_url = soup.find('img')['src']
+
+# Rating needed to be extracted from the class
+def has_class(tag):
+    return tag and 'star-rating' in tag
+rating = soup.find_all('p', class_=has_class)
+for thing in rating:
+    print(thing)
+with open('book_file.csv', mode='w') as books:
+    books_writer = csv.writer(books, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    books_writer.writerow(["product_page_url", "category", "book title", "review_rating", "image_url", "product_description", 
+                           "upc",  "price_including_tax", "price_excluding_tax", "quantity_available"])
+    books_writer.writerow([product_page_url, category_of_book, title, 5, image_url, 
+                           product_description, upc, price_including_tax, price_excluding_tax, quantity_available])
