fbeaadf2bd6fb04ce6274e7f03a287cc40056730
Michael Kucukarslan <michaelkucukarslan@gmail.com>
2024-02-10 20:33:08 -0800

    Got code into functions to reuse and got book info into a list to get them into a CSV file

--- Book_scraper.py
+++ Book_scraper.py
@@ -1,30 +1,36 @@
 import requests, csv
 from bs4 import BeautifulSoup
 
-URL = "http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"
-page = requests.get(URL)
+# URL = "http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"
+# page = requests.get(URL)
+class Book_scraper:
+# Create a function that scrapes the book's info
+    def web_address_to_book_info_list(self, url_address):
+        print(url_address)
+        page = requests.get(url_address)
+        soup = BeautifulSoup(page.text, 'html.parser')
+        # Most of the needed information is in the table towards the bottom. Find all the tds and place them in the appropriate section
+        columns = soup.find_all('td')
+        upc = columns[0].text.strip()
+        price_including_tax = columns[2].text.strip()
+        price_excluding_tax = columns[3].text.strip()
+        quantity_available = columns[5].text.strip()
 
-soup = BeautifulSoup(page.text, 'html.parser')
-
-# Most of the needed information is in the table towards the bottom. Find all the tds and place them in the appropriate section
-columns = soup.find_all('td')
-upc = columns[0].text.strip()
-price_including_tax = columns[2].text.strip()
-price_excluding_tax = columns[3].text.strip()
-quantity_available = columns[5].text.strip()
-
-# Other information was around the html page
-product_page_url = URL
-category_of_book = soup.find_all('a')[3].text
-title = soup.find('h1').text 
-product_description = soup.find_all('p')[3].text 
-image_url = soup.find('img')['src']
-rating = soup.find('p', class_="star-rating").get("class")[1]
+        # Other information was around the html page
+        product_page_url = url_address
+        category_of_book = soup.find_all('a')[3].text
+        title = soup.find('h1').text 
+        product_description = soup.find_all('p')[3].text 
+        image_url = soup.find('img')['src']
+        rating = soup.find('p', class_="star-rating").get("class")[1]
+        return [product_page_url, category_of_book, title, rating, image_url, 
+                            product_description, upc, price_including_tax, price_excluding_tax, quantity_available]    
 
 
-with open('book_file.csv', mode='w') as books:
-    books_writer = csv.writer(books, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    books_writer.writerow(["product_page_url", "category", "book title", "review_rating", "image_url", "product_description", 
-                           "upc",  "price_including_tax", "price_excluding_tax", "quantity_available"])
-    books_writer.writerow([product_page_url, category_of_book, title, rating, image_url, 
-                           product_description, upc, price_including_tax, price_excluding_tax, quantity_available])
+
+# with open('book_file.csv', mode='w') as books:
+#     books_writer = csv.writer(books, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+#     books_writer.writerow(["product_page_url", "category", "book title", "review_rating", "image_url", "product_description", 
+#                            "upc",  "price_including_tax", "price_excluding_tax", "quantity_available"])
+#     books_writer.writerow([product_page_url, category_of_book, title, rating, image_url, 
+#                            product_description, upc, price_including_tax, price_excluding_tax, quantity_available])
--- Main.py
+++ Main.py
@@ -1,28 +1,29 @@
 # This main file will find all the main categories on the online bookstore
 from bs4 import BeautifulSoup
 import requests
+from Book_scraper import Book_scraper
 
 url = "http://books.toscrape.com/"
 web_page = requests.get(url)
-soup = BeautifulSoup(web_page.text, "html.parser")
 
+soup = BeautifulSoup(web_page.text, "html.parser")
 categories = soup.find(class_="side_categories").find_all('a')
 
 # get all categories from the main page
+# TODO: Remove category list if no longer needed
 book_categories_web_address = []
 book_categories_type = []
-for item in categories:
-    book_categories_web_address.append(item.get("href"))
-    book_categories_type.append(item.text.strip())
-# The first one is the sample page which doesn't need to be saved
+for book in categories:
+    book_categories_web_address.append(book.get("href"))
+    book_categories_type.append(book.text.strip())
+# The first one is the sample page which doesn't need to be saved so pop it
 book_categories_web_address.pop(0) 
 book_categories_type.pop(0)
 
-# This is code that will get one page of books from one category
+# This is code that will get all pages of books from one category
 big_book_list = []
-first_category = url + book_categories_web_address[3]
+first_category = url + book_categories_web_address[0]
 def get_links_for_one_category(site_url):
-    print("First site: " + site_url)
     page_two = requests.get(site_url)
     soup_2 = BeautifulSoup(page_two.text, "html.parser")
     # .product_pod h3 a "These are where the links are"
@@ -30,26 +31,27 @@ def get_links_for_one_category(site_url):
         text_link = link.get('href')
         text_link = url + "catalogue" + text_link[8:]
         big_book_list.append(text_link)
-
+    # Check for more books on next page
     next_page = soup_2.select('.pager .next a')
     if next_page:
-        list_of_backspaces = find(site_url, '/')
-        new_page = site_url[:list_of_backspaces[-1]] + '/' +  next_page[0].get('href')
-        # http://books.toscrape.com/catalogue/category/books/mystery_3/index.html
-        # http://books.toscrape.com/catalogue/category/books/mystery_3/page-2.html
-        print(new_page)
+        list_of_backspaces = find_all_char_pos_in_string(site_url, '/')
+        new_page = site_url[:list_of_backspaces[-1]] + '/' + next_page[0].get('href')
         get_links_for_one_category(new_page)
-# Figure out Pagination
-def find(string, character):
+
+def find_all_char_pos_in_string(string, character):
     return [i for i, ltr in enumerate(string) if ltr == character]
 
 get_links_for_one_category(first_category)
-for item in big_book_list:
-    print(item)
 
-print("good job")
+# TODO: Create function that takes a url and get back a list to put into a csv file
+all_books_info = []
+bs = Book_scraper()
+for book in big_book_list:
+    all_books_info.append(bs.web_address_to_book_info_list(book))
+
+for book in all_books_info:
+    print(book)
 # Do milestone 3
-# TODO: turn code into functions
 # TODO: get the category saved to go into the csv file
 # TODO: get information from those books
 # TODO: get the next page if available
