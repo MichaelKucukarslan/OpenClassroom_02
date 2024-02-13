b78f7d685d79da473cf337e267fb8d268ffb2d92
Michael Kucukarslan <michaelkucukarslan@gmail.com>
2024-02-12 16:00:16 -0800

    Got all tasks done. Still needs cleaning up

--- Main.py
+++ Main.py
@@ -1,6 +1,6 @@
 # This main file will find all the main categories on the online bookstore
 from bs4 import BeautifulSoup
-import requests
+import requests, csv
 from Book_scraper import Book_scraper
 
 url = "http://books.toscrape.com/"
@@ -18,7 +18,6 @@ book_categories_web_address.pop(0)
 
 # This is code that will get all pages of books from one category
 big_book_list = []
-first_category = url + book_categories_web_address[0]
 def get_links_for_one_category(site_url):
     page_two = requests.get(site_url)
     soup_2 = BeautifulSoup(page_two.text, "html.parser")
@@ -37,16 +36,27 @@ def get_links_for_one_category(site_url):
 def find_all_char_pos_in_string(string, character):
     return [i for i, ltr in enumerate(string) if ltr == character]
 
-get_links_for_one_category(first_category)
+first_category = url + book_categories_web_address[0]
+for item in book_categories_web_address:
+        get_links_for_one_category(url + item)
 
 # TODO: Create function that takes a url and get back a list to put into a csv file
 all_books_info = []
 bs = Book_scraper()
-for book in big_book_list:
-    all_books_info.append(bs.web_address_to_book_info_list(book))
+# for book in big_book_list:
+#     all_books_info.append(bs.web_address_to_book_info_list(book))
 
-for book in all_books_info:
-    print(book)
+# for book in all_books_info:
+#     print(book)
+
+
+with open('book_file.csv', mode='w') as books:
+    books_writer = csv.writer(books, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    books_writer.writerow(["product_page_url", "category", "book title", "review_rating", "image_url", "product_description", 
+                           "upc",  "price_including_tax", "price_excluding_tax", "quantity_available"])
+    for item in big_book_list:
+        books_writer.writerow(bs.web_address_to_book_info_list(item))
+
 # Do milestone 3
 # TODO: get the category saved to go into the csv file
 # TODO: get information from those books
